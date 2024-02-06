f94eba4610dd178044ba2848b1dcaee70050102e
yiju_wu <yiju_wu@brogent.com>
2024-02-06 23:58:16 +0800

    Add gamma_process_lib.py
    This module includes functions for image gamma processing (or histogram
    processing).

---
+++ src/gamma_process_lib.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue 06 Feb 2024
+
+Author: YIJU WU (gn02129251)
+
+File: gamma_process_lib.py
+
+Topic: This module includes functions for image gamma processing (or histogram processing).
+
+"""
+
+
+def main(*args):
+    return 0
+
+
+if __name__ == "__main__":
+    main()
