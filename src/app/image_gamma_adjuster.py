982fdc9df81006f470505514bfed89dfcabc884d
yiju_wu <yiju_wu@brogent.com>
2024-02-07 00:19:34 +0800

    Add image_gamma_adjuster.py

---
+++ src/app/image_gamma_adjuster.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed 07 Feb 2024
+
+Author: YIJU WU (gn02129251)
+
+File: image_gamma_adjuster.py
+
+Topic: An application for adjusting image gamma
+
+"""
+
+
+def main(*args):
+    return 0
+
+
+if __name__ == "__main__":
+    main()
