caca0fbffd553d7df8940e9cf971e15ed00f33f1
yiju_wu <yiju_wu@brogent.com>
2024-02-07 00:15:44 +0800

    Add app.py

---
+++ src/app/app.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed 07 Feb 2024
+
+Author: YIJU WU (gn02129251)
+
+File: app.py
+
+Topic: This module includes a template class for application development.
+
+"""
+
+
+def main(*args):
+    return 0
+
+
+if __name__ == "__main__":
+    main()
