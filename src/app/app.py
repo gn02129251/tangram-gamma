4d79ce82e31aa8103edc0f81526e20077ce7a8cb
yiju_wu <yiju_wu@brogent.com>
2024-02-07 00:17:00 +0800

    feat: add App class
    Add App class as the 1st fundamental class for application development.

--- src/app/app.py
+++ src/app/app.py
@@ -9,6 +9,23 @@ File: app.py
 Topic: This module includes a template class for application development.
 
 """
+import tkinter as tk
+
+
+class App(tk.TK):
+    def __init__(self, title, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.title(title)
+        self.geometry("800x600")
+        self.resizable(width=False, height=False)
+        self.create_widgets()
+
+        # Configure padding for all child widgets to optimize application apperance
+        for child in self.winfo_children():
+            child.grid_configure(padx=5, pady=5)
+
+    def create_widgets(self):
+        pass  # Overwrite in subclass
 
 
 def main(*args):
